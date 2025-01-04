068b121951465f99b5e53344433b5a7bf398ec04
Rahmankhattak711 <rahmankhattak796@gmail.com>
2025-01-04 15:50:54 +0500

    initial commit in sociallywebapp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
+        dmSerifText: ["var(--font-dm-serif-text)"],
+      },
     },
   },
   plugins: [],
